98b3120b0964cc857a4a7583214090948c1607a2
OZON-SIBERIA <medvedev152@live.com>
2019-11-06 13:15:52 +1000

    add jupynote

--- main.py
+++ main.py
@@ -6,34 +6,55 @@ def distance(x1, x2, y1, y2, z1, z2, w1, w2):
     return math.sqrt(math.pow((x2-x1), 2)+math.pow((y2-y1), 2)+math.pow((z2-z1), 2)+math.pow((w2-w1), 2))
 
 
-reading = open("data.json", "r")
-jsonstring = reading.read()
-reading.close()
-data = json.loads(jsonstring)
+def jsontopython():
+    reading = open("data.json", "r")
+    jsonstring = reading.read()
+    reading.close()
+    data = json.loads(jsonstring)
+    return data
 
+
+data = jsontopython()
 keys = data.keys()
-dictMain = {}
+dictmain = {}
+dictmap = {}
 for i in keys:
-    keyA = str(i)
-    stringA = data[keyA]
-    dictAB = {}
-    listAB = []
+    keya = str(i)
+    stringa = data[keya]
+    dictab = {}
+    listab = []
     for key in keys:
         if key != i:
-            keyB = str(key)
-            stringB = data[keyB]
-            classB = stringB["class"]
-            f1A = float(stringA["f1"].replace(',', '.'))
-            f1B = float(stringB["f1"].replace(',', '.'))
-            f2A = float(stringA["f2"].replace(',', '.'))
-            f2B = float(stringB["f2"].replace(',', '.'))
-            f3A = float(stringA["f3"].replace(',', '.'))
-            f3B = float(stringB["f3"].replace(',', '.'))
-            f4A = float(stringA["f4"].replace(',', '.'))
-            f4B = float(stringB["f4"].replace(',', '.'))
-            distanceAB = distance(f1A, f1B, f2A, f2B, f3A, f3B, f4A, f4B)
-            listAB.append(distanceAB)
-            dictAB[distanceAB] = classB
-    print(dictAB)
-    dictMain[i] = dictAB
-print(dictMain)
+            keyb = str(key)
+            stringb = data[keyb]
+            classb = stringb["class"]
+            f1a = float(stringa["f1"].replace(',', '.'))
+            f1b = float(stringb["f1"].replace(',', '.'))
+            f2a = float(stringa["f2"].replace(',', '.'))
+            f2b = float(stringb["f2"].replace(',', '.'))
+            f3a = float(stringa["f3"].replace(',', '.'))
+            f3b = float(stringb["f3"].replace(',', '.'))
+            f4a = float(stringa["f4"].replace(',', '.'))
+            f4b = float(stringb["f4"].replace(',', '.'))
+            distanceab = distance(f1a, f1b, f2a, f2b, f3a, f3b, f4a, f4b)
+            listab.append(distanceab)
+            dictab[distanceab] = classb
+    print(type(dictab))
+    listab = sorted(listab)
+    dictmap[i] = listab
+    dictmain[i] = dictab
+
+
+k = 30
+for c in enumerate(k):
+
+
+"""y1 = errorfunction()
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1, aspect=1)
+ax.set_xlim(0, 4)
+ax.set_ylim(0, 4)
+ax.plot(x, y1, c=(0.25, 0.25, 1.00), lw=2, label="Blue signal", zorder=10)
+ax.set_xlabel("X axis label")
+ax.set_ylabel("Y axis label")
+plt.show()"""
